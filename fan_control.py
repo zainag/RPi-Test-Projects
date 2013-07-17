018fa919740f3d78fdddb5b0ef6b707a8d648b3a
Rahul <zainag77@gmail.com>
2013-07-17 16:21:33 -0400

    Updated code, logging and daemoning still remaining

--- fan_control.py
+++ fan_control.py
@@ -1,12 +1,55 @@
 #!/usr/bin/env python
 # Fan control mod for Raspberry Pi
 
-import RPi.GPIO as GPIO, time,  os 
+import RPi.GPIO as GPIO, time, datetime, subprocess, os 
 
 DEBUG = 1 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, GPIO.HIGH)
-time.sleep(15)
-GPIO.output(18, GPIO.LOW)
-GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+# Respective ports on the GPIO header 
+FAST = 18
+SLOW = 25
+
+# Default settings for fan control
+MAX_TEMP = 50
+MIN_TEMP = 40
+POLL_TIME = 5
+ 
+GPIO.setup(FAST, GPIO.OUT)
+GPIO.setup(SLOW, GPIO.OUT)
+
+def get_temperature():
+    # Returns the temperature in degrees C
+	try:
+  		s = subprocess.check_output(["vcgencmd","measure_temp"])
+  		return float(s.split("=")[1][:-3])
+	except:
+		# Something went wrong keep the fan on high
+		return MAX_TEMP+1
+		
+try:
+	while True:
+		current_temp = get_temperature()
+		print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - current temp is ' + str(current_temp)
+		
+		if	current_temp > MAX_TEMP:
+			print 'Setting fan speed to HIGH'
+			GPIO.output(SLOW, GPIO.LOW)
+			GPIO.output(FAST, GPIO.HIGH)
+			POLL_TIME = 5
+		elif (current_temp <= MAX_TEMP) and (current_temp > MIN_TEMP):
+			print 'Setting fan speed to LOW'
+			GPIO.output(SLOW, GPIO.HIGH)
+			GPIO.output(FAST, GPIO.LOW)
+			POLL_TIME = 10
+		else:
+			print 'Turn the fan off!'
+			GPIO.output(SLOW, GPIO.LOW)
+			GPIO.output(FAST, GPIO.LOW)
+			POLL_TIME = 15
+		time.sleep(POLL_TIME)
+		
+except KeyboardInterrupt:
+	print'\nCtrl-C pressed, exiting now!'
+finally:
+	GPIO.cleanup()
